d0188be8be4b911cb94af626f9fb04ebdad73840
ibrahim <ibravoh149@gmail.com>
2019-06-25 11:58:40 +0100

    renamed tabs

--- src/Component/Overview.js
+++ src/Component/Overview.js
@@ -22,7 +22,7 @@ import React, { Component } from 'react';
 // export default Overview
 
 export const Overview = ({ data }) => {
-    const {imageUrl, description, title}=data
+    const { imageUrl, description, title } = data
     return (
         <div className="row">
             <div className="col-12 col-md-4 align-items-center">
